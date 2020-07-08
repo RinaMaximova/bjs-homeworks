f3871a2fbdb7ab445ec9b2f4490d47e17e2b6c7b
Karina Maksimova <maksimova.cari@yandex.ru>
2020-07-08 19:24:56 +0300

    1.3-data-types

--- 1.3-data-types/task.js
+++ 1.3-data-types/task.js
@@ -1,9 +1,30 @@
 function calculateTotalMortgage(percent, contribution, amount, date) {
-    // код для задачи №1 писать здесь
-    // return totalAmount;
+    if (typeof percent !== 'number' && typeof percent !== 'string')
+        return `Параметр percent содержит неправильное значение ${percent}`;
+
+    if (typeof contribution !== 'number' && typeof contribution !== 'string')
+        return `Параметр contribution содержит неправильное значение ${contribution}`;
+
+    if (typeof amount !== 'number' && typeof amount !== 'string')
+        return `Параметр amount содержит неправильное значение ${amount}`;
+
+    currentDate = new Date();
+
+    contribution = parseInt(contribution);
+    percent = parseInt(percent);
+    amount = parseInt(amount);
+
+    mortgagePercent = percent / 100 / 12;
+    mortgageAmount = amount - contribution;
+    mortgageMonths = date.getMonth() - currentDate.getMonth() + (12 * (date.getFullYear() - currentDate.getFullYear()));
+
+    mortgageResult = (mortgageAmount * (mortgagePercent + mortgagePercent / (((1 + mortgagePercent) ** mortgageMonths) - 1))) * mortgageMonths;
+
+    return new Number(mortgageResult.toFixed(2));
 }
 
 function getGreeting(name) {
-    // код для задачи №2 писать здесь
-    // return greeting;
+   name = name || 'Аноним';
+
+   return `Привет, мир! Меня зовут ${name}`
 }
