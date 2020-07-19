adc0d87186564da99522179b9d89d062e7902a9a
Karina Maksimova <maksimova.cari@yandex.ru>
2020-07-19 21:53:45 +0300

    2.2-object-prototype

--- 2.2-object-prototype/task.js
+++ 2.2-object-prototype/task.js
@@ -1,8 +1,27 @@
-//String.prototype.isPalindrome - для задачи №1
+String.prototype.isPalindrome = function () {
+    let str = this.toLowerCase().split(" ").join('');
+    let reverseStr = str.split('').reverse().join("");
+
+    return (str === reverseStr)
+};
 
 function getAverageMark(marks) {
-    // код для задачи №2 писать здесь
-    // return averageMark
+    let sum = 0,
+        average = 0,
+        roundedAverage;
+
+    if (!marks.length)
+        return average;
+
+    for (let mark of marks.values()) {
+        sum += mark;
+    }
+
+    average = sum / marks.length;
+
+    roundedAverage = Math.round(average);
+
+    return roundedAverage;
 }
 
 function checkBirthday(birthday) {
